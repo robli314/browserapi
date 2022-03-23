e6c92fd2e43510809511cb3425eeaf0525e7f166
Robson Liebke <robsonliebke@gmail.com>
2022-03-24 00:30:43 +0100

    learning about async and defer

---
+++ script2.js
@@ -0,0 +1,11 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function run() {
+    await new Promise(r => setTimeout(r, 8000));
+}
+
+run();
+
+console.log(document.getElementById('test2'));
